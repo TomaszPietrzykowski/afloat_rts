bd6d7c89ae652b82719e026fb84c8253f449d8d5
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2024-05-12 22:04:36 +0200

    game object placement with scriptable objects added #AB134

--- Assets/_Scripts/PlacementSystem.cs
+++ Assets/_Scripts/PlacementSystem.cs
@@ -11,11 +11,65 @@ public class PlacementSystem : MonoBehaviour
     [SerializeField]
     private Grid grid;
 
+    [SerializeField]
+    private ObjectDatabaseSO database;
+
+    private int selectedObjectIndex = -1;
+
+    [SerializeField]
+    private GameObject gridVisualization;
+
+    private void Start()
+    {
+        StopPlacement();
+    }
+
     private void Update()
     {
+        if (selectedObjectIndex < 0)
+        {
+            return;
+        }
+
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
         Vector3Int gridPosition = grid.WorldToCell(mousePosition);
         mouseIndicator.transform.position = mousePosition;
         cellIndicator.transform.position = grid.CellToWorld(gridPosition);
     }
+
+    private void PlaceStructure()
+    {
+        if (inputManager.IsPointerOverUi())
+        {
+            return;
+        }
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+        GameObject newObject = Instantiate(database.objectsData[selectedObjectIndex].Prefab);
+        newObject.transform.position = grid.CellToWorld(gridPosition);
+    }
+
+    public void StartPlacement(int Id)
+    {
+        StopPlacement();
+        selectedObjectIndex = database.objectsData.FindIndex(data => data.Id == Id);
+        if (selectedObjectIndex < 0)
+        {
+            Debug.LogError($"Could not find object with id: {Id}.");
+            return;
+        }
+        gridVisualization.SetActive(true);
+        cellIndicator.SetActive(true);
+        inputManager.OnClicked += PlaceStructure;
+        inputManager.OnExit += StopPlacement;
+    }
+    private void StopPlacement()
+    {
+        selectedObjectIndex = -1;
+        gridVisualization.SetActive(false);
+        cellIndicator.SetActive(false);
+        inputManager.OnClicked -= PlaceStructure;
+        inputManager.OnExit -= StopPlacement;
+    }
+
 }
