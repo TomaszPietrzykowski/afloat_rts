16d7bce23110d40ac8237670cc77991d3d73aa0c
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2024-05-14 23:14:07 +0200

    collision detection added to structure placement AB#136

--- Assets/_Scripts/PlacementSystem.cs
+++ Assets/_Scripts/PlacementSystem.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlacementSystem : MonoBehaviour
@@ -22,35 +23,60 @@ public class PlacementSystem : MonoBehaviour
     [SerializeField]
     private AudioSource source;
 
+    private GridData floorData, furnitureData;
+
+    private Renderer previewRenderer;
+
+    private List<GameObject> placedGameObjects = new();
+
     private void Start()
     {
         StopPlacement();
+        floorData = new GridData();
+        furnitureData = new GridData();
+        previewRenderer = cellIndicator.GetComponentInChildren<Renderer>();
     }
 
     private void Update()
     {
-        if (selectedObjectIndex < 0)
-        {
-            return;
-        }
+        if (selectedObjectIndex < 0) return;
 
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
         Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+
+        bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
+        previewRenderer.material.color = placementValidity ? Color.white : Color.red;
+
         mouseIndicator.transform.position = mousePosition;
         cellIndicator.transform.position = grid.CellToWorld(gridPosition);
     }
 
     private void PlaceStructure()
     {
-        if (inputManager.IsPointerOverUi())
-        {
-            return;
-        }
-        source.Play();
+        if (inputManager.IsPointerOverUi()) return;
+
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
         Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+
+        bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
+        if (placementValidity == false) return;
+
+        source.Play();
         GameObject newObject = Instantiate(database.objectsData[selectedObjectIndex].Prefab);
         newObject.transform.position = grid.CellToWorld(gridPosition);
+        placedGameObjects.Add(newObject);
+        GridData selectedData = database.objectsData[selectedObjectIndex].Id == 0 ? floorData : furnitureData;
+        selectedData.AddObjectAt(gridPosition,
+                                 database.objectsData[selectedObjectIndex].Size,
+                                 database.objectsData[selectedObjectIndex].Id,
+                                 placedGameObjects.Count - 1); ;
+    }
+
+    private bool CheckPlacementValidity(Vector3Int gridPosition, int selectedObjectIndex)
+    {
+        GridData selectedData = database.objectsData[selectedObjectIndex].Id == 0 ? floorData : furnitureData;
+
+        return selectedData.CanPlaceObjectAt(gridPosition, database.objectsData[selectedObjectIndex].Size);
     }
 
     public void StartPlacement(int Id)
